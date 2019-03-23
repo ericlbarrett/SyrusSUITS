64d6b6a926fdf0b43cc1c01611e185267d7b9188
Harley <harley.vasquez>
2019-03-22 20:45:09 -0700

    Added Navigation Files

--- SyrusSUITS/Assets/Scripts/NavigationService.cs
+++ SyrusSUITS/Assets/Scripts/NavigationService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,4 +14,5 @@ public class NavigationService : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
