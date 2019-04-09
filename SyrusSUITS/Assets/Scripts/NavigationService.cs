1ab4fc96044940a5ffc323d4eaf777c310b380bd
Harley <harley.vasquez>
2019-04-09 14:31:04 -0700

    Fixed indentation on NavigationService.cs

--- SyrusSUITS/Assets/Scripts/NavigationService.cs
+++ SyrusSUITS/Assets/Scripts/NavigationService.cs
@@ -7,22 +7,22 @@ using UnityEngine;
 
 public class NavigationService : MonoBehaviour {
 
-  public static List<Node> nodeMap = new List<Node>();
-  public static List<Node> route = new List<Node>();
+    public static List<Node> nodeMap = new List<Node>();
+    public static List<Node> route = new List<Node>();
 
-	private static NavigationService _Instance;
+    private static NavigationService _Instance;
   
-  public static NavigationService Instance
-  {
-      get
-      {
-          if (_Instance == null)
-          {
-              _Instance = FindObjectOfType<NavigationService>();
-          }
-          return _Instance;
-      }
-  }
+    public static NavigationService Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = FindObjectOfType<NavigationService>();
+            }
+            return _Instance;
+        }
+    }
   
 	Vector3 navOrigin;
 	Quaternion navRotation;
@@ -33,11 +33,11 @@ public class NavigationService : MonoBehaviour {
 	void Start () {
 		_Instance = this;
     
-    LoadFromJson("/NodeMaps/sampleMap_1.json");
+        LoadFromJson("/NodeMaps/sampleMap_1.json");
 
-    route = GetRoute(GetNodeByID(1), GetNodeByID(6));
+        route = GetRoute(GetNodeByID(1), GetNodeByID(6));
 
-    LogRoute(route);
+        LogRoute(route);
 	}
 	
 	// Update is called once per frame
@@ -53,97 +53,97 @@ public class NavigationService : MonoBehaviour {
 		Debug.Log("Nav Origin Set");
 	}
 
-  public List<Node> GetRoute(Node source, Node destination)
-  {
-      Pathfinder pathFinder = new Pathfinder(nodeMap, source, destination);
-      pathFinder.Execute();
+    public List<Node> GetRoute(Node source, Node destination)
+    {
+        Pathfinder pathFinder = new Pathfinder(nodeMap, source, destination);
+        pathFinder.Execute();
 
-      return pathFinder.GetShortestPath();
-  }
+        return pathFinder.GetShortestPath();
+    }
 
-  public Node GetNodeByID(int id)
-  {
-      return nodeMap.Find(x => x.id.Equals(id));
-  }
+    public Node GetNodeByID(int id)
+    {
+        return nodeMap.Find(x => x.id.Equals(id));
+    }
 
-  void LogNodes()
-  {
-      foreach (Node node in nodeMap)
-      {
-          Debug.Log(node.id + " (" + node.position.x + ", " + node.position.y + ", " + node.position.z + ")");
-      }
-  }
+    void LogNodes()
+    {
+        foreach (Node node in nodeMap)
+        {
+            Debug.Log(node.id + " (" + node.position.x + ", " + node.position.y + ", " + node.position.z + ")");
+        }
+    }
 
-  public void LogRoute(List<Node> route)
-  {
-      string output = "Route: ";
+    public void LogRoute(List<Node> route)
+    {
+        string output = "Route: ";
 
-      for (int i = 0; i < route.Count; i++)
-      {
-          output += route[i].id;
+        for (int i = 0; i < route.Count; i++)
+        {
+            output += route[i].id;
 
-          if (i < route.Count - 1) output += " -> ";
-      }
+            if (i < route.Count - 1) output += " -> ";
+        }
 
-      Debug.Log(output);
-  }
+        Debug.Log(output);
+    }
 
-  void LoadFromJson(string directory)
-  {
-      string filePath = Application.streamingAssetsPath + directory;
+    void LoadFromJson(string directory)
+    {
+        string filePath = Application.streamingAssetsPath + directory;
 
-      if (File.Exists(filePath))
-      {
-          string jsonContent = File.ReadAllText(filePath);
-          NodeMap jsonNodeMap = JsonUtility.FromJson<NodeMap>(jsonContent);
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            NodeMap jsonNodeMap = JsonUtility.FromJson<NodeMap>(jsonContent);
 
-          nodeMap = ConvertedJsonNodeMap(jsonNodeMap);
-      }
-      else
-      {
-          Debug.LogError("Cannot load data from " + filePath);
-      }
-  }
+            nodeMap = ConvertedJsonNodeMap(jsonNodeMap);
+        }
+        else
+        {
+            Debug.LogError("Cannot load data from " + filePath);
+        }
+    }
 
-  public List<Node> ConvertedJsonNodeMap(NodeMap jsonNodeMap)
-  {
-      List<Node> nodeMap = new List<Node>();
+    public List<Node> ConvertedJsonNodeMap(NodeMap jsonNodeMap)
+    {
+        List<Node> nodeMap = new List<Node>();
 
-      foreach (JsonNode jsonNode in jsonNodeMap.nodes)
-      {
-          Node node = new Node();
+        foreach (JsonNode jsonNode in jsonNodeMap.nodes)
+        {
+            Node node = new Node();
 
-          node.id = jsonNode.id;
-          node.position = new Vector3(jsonNode.position.x, jsonNode.position.y, jsonNode.position.z);
-          node.adjacentNodeIDs = jsonNode.adjacentNodeIDs;
+            node.id = jsonNode.id;
+            node.position = new Vector3(jsonNode.position.x, jsonNode.position.y, jsonNode.position.z);
+            node.adjacentNodeIDs = jsonNode.adjacentNodeIDs;
 
-          nodeMap.Add(node);
-      }
+            nodeMap.Add(node);
+        }
 
-      return nodeMap;
-  }
+        return nodeMap;
+    }
 
-  [Serializable]
-  public class NodeMap
-  {
-      public string title;
-      public List<JsonNode> nodes;
-  }
+    [Serializable]
+    public class NodeMap
+    {
+        public string title;
+        public List<JsonNode> nodes;
+    }
 
-  [Serializable]
-  public class JsonNode
-  {
-      public int id;
-      public Positition position;
-      public List<int> adjacentNodeIDs;
+    [Serializable]
+    public class JsonNode
+    {
+        public int id;
+        public Positition position;
+        public List<int> adjacentNodeIDs;
 
-  }
+    }
 
-  [Serializable]
-  public class Positition
-  {
-      public float x;
-      public float y;
-      public float z;
-  }
+    [Serializable]
+    public class Positition
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
 }
