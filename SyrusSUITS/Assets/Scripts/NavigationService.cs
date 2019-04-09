a262a753adfb8f0656b7387c2e1b31ebbdffa3c5
RCC_ACM <rcchololens@outlook.com>
2019-04-09 12:49:41 -0700

    Added Image Targets

--- SyrusSUITS/Assets/Scripts/NavigationService.cs
+++ SyrusSUITS/Assets/Scripts/NavigationService.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 
 public class NavigationService : MonoBehaviour {
 
+	private static NavigationService _Instance;
+    public static NavigationService Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = FindObjectOfType<NavigationService>();
+            }
+            return _Instance;
+        }
+    }
 	Vector3 navOrigin;
 	Quaternion navRotation;
 
+	public bool calibrated = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		_Instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void SetOrigin(Vector3 origin, Quaternion rotation) {
+		navOrigin = origin;
+		navRotation = rotation;
+		calibrated = true;
+		Debug.Log("Nav Origin Set");
 	}
 
 }
