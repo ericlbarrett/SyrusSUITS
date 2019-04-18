46baf55ec29b4ffaecae13c61c730aafff4a6f20
CE99 <49452990+CE99@users.noreply.github.com>
2019-04-17 22:13:35 -0700

    Added liquid pressure script

---
+++ SyrusSUITS/Assets/Scripts/LeapManager.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if !UNITY_EDITOR
+using SyrusLeapClient;
+#endif
+
+public class LeapManager : MonoBehaviour {
+
+    GameObject UIleft, UIright;
+
+    private static LeapManager _Instance;
+    public static LeapManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = FindObjectOfType<LeapManager>();
+            }
+            return _Instance;
+        }
+    }
+
+    public delegate void GestureTap();
+    public static event GestureTap OnGestureTap;
+
+    public delegate void GestureSwipe();
+    public static event GestureSwipe OnGestureSwipe;
+    
+	#if !UNITY_EDITOR
+    ///Client manager 
+	ClientBTManager cbm;
+
+    Camera mainCamera;
+    //Vector for all the finger points
+    Vector3[] points;
+    //array of ojects
+    GameObject[] objs;
+    //accounts for the offset of the leap on the hololens
+    Vector3 leapOffset = new Vector3(0.0f, 0.08f, 0.04f);
+
+    Quaternion rotation = Quaternion.Euler(110.0f, 0.0f, 0.0f);
+    Hand left, right;
+
+	// Use this for initialization
+	void Start () {
+        //Defaults the UI to be Invisible
+        UIright.setActive(false);
+        UIleft.setActive(false);
+        //Server Manager Object
+		cbm = new ClientBTManager();
+        //Packets
+        cbm.PacketReceived += Recieved;
+        //connected
+        cbm.Connected += OnConnect;
+        cbm.Initialize();
+        
+        mainCamera = Camera.main;
+        //gives indexed for each index
+        points = new Vector3[9];
+        objs = new GameObject[9];
+
+        //fills each vector array
+        for (int i = 0; i < 9; i++) {
+            //creates a sphere
+            objs[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //size of the object
+            objs[i].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < 9; i++) {
+            Vector3 p = leapOffset + rotation * points[i];
+            objs[i].transform.position = ToUnityCoords(points[i]);//mainCamera.transform.position + mainCamera.transform.rotation * p;
+        }
+
+        //finds angle for area that the UI for the left hand will be visible
+        if (Vector3.angle(points[1], -mainCamera.transform.forward) < 20.0f) {
+            //sets the UI as visible when the parameters are met
+            UIleft.setActive(true);
+        }else{
+            //sets the UI as invisible when the parameters are met
+            UIleft.setActive(false);
+        }
+        
+        //finds angle for the area that the UI for the right hand will be visible
+        if(Vector3.angle(points[3], -mainCamera.transform.forward) < 20.0f){
+            //sets the UI as visible when the parameters are met
+            UIright.setActive(true);
+        }else{
+            //sets the UI as invisible when the parameters are met
+            UIright.setActive(false);
+        }
+		
+	}
+
+    public Vector3 ToUnityCoords(Vector3 p) {
+        return mainCamera.transform.position + mainCamera.transform.rotation * (leapOffset + rotation * p);
+    }
+
+    public Vector3 ToUnityCoordsDir(Vector3 p) {
+        return (mainCamera.transform.rotation * rotation * p).normalized;
+    }
+
+    private async void OnConnect() {
+        Debug.Log("Connected");
+    }
+
+    private Vector3 readVector(byte[] arr, int offset) {
+        float x = BitConverter.ToSingle(arr, offset);
+        float y = BitConverter.ToSingle(arr, offset + 4);
+        float z = BitConverter.ToSingle(arr, offset + 8);
+
+        return new Vector3(-x / 1000.0f, y / 1000.0f, z / 1000.0f);
+    }
+
+	private async void Recieved(SyrusPacket packet) {
+            switch (packet.id) {
+                case 20: // Update message
+                    {
+                        for (int i = 0; i < 9; i++) {
+                            points[i] = readVector(packet.data, 12 * i);
+                        }
+                        
+                        break;
+                    }
+                case 21: // Screen tap gesture
+                    {
+                        Debug.Log("Gesture: Tap");
+                        Vector3 pos = readVector(packet.data, 0);
+                        Vector3 dir = readVector(packet.data, 12).normalized;
+                        
+                        break;
+                    }
+                case 22: // Swipe gesture
+                    {
+                        Debug.Log("Gesture: Swipe");
+                        Vector3 pos = readVector(packet.data, 0);
+                        Vector3 dir = readVector(packet.data, 12).normalized;
+                        break;
+                    }
+            }
+        }
+
+	#endif
+
+    class Hand {
+        Vector3 palmPos;
+        Vector3 palmNorm;
+        Vector3 indexTip;
+        Vector3 indexDir;
+    }
+
+}
+
--- SyrusSUITS/Assets/Scripts/NavigationService.cs
+++ SyrusSUITS/Assets/Scripts/NavigationService.cs
@@ -1,16 +1,50 @@
-﻿using Assets.Scripts;
+﻿using System;
+using Assets.Scripts;
 using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NavigationService : MonoBehaviour {
 
+    public static List<Node> nodeMap = new List<Node>();
+    public static List<Location> locations = new List<Location>();
+    public static List<Node> route = new List<Node>();
+
+    private static NavigationService _Instance;
+  
+    public static NavigationService Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = FindObjectOfType<NavigationService>();
+            }
+            return _Instance;
+        }
+    }
+  
 	Vector3 navOrigin;
 	Quaternion navRotation;
 
+	public bool calibrated = false;
+    public string modelName;
+
+    public delegate void MapLoadedEvent();
+    public event MapLoadedEvent MapLoaded;
+
+    void Awake() {
+        _Instance = this;
+    }
+
 	// Use this for initialization
 	void Start () {
-		
+        LoadFromJson("/NodeMaps/ISS.json");
+
+        route = GetRoute(GetNodeByID(1), GetNodeByID(7));
+
+        LogRoute(route);
 	}
 	
 	// Update is called once per frame
@@ -18,4 +52,162 @@ public class NavigationService : MonoBehaviour {
 		
 	}
 
+
+	public void SetOrigin(Vector3 origin, Quaternion rotation) {
+		navOrigin = origin;
+		navRotation = rotation;
+		calibrated = true;
+		Debug.Log("Nav Origin Set");
+	}
+
+    public List<Node> GetRoute(Node source, Node destination)
+    {
+        Pathfinder pathFinder = new Pathfinder(nodeMap, source, destination);
+        pathFinder.Execute();
+
+        return pathFinder.GetShortestPath();
+    }
+
+    public Node GetNodeByID(int id)
+    {
+        return nodeMap.Find(x => x.id.Equals(id));
+    }
+
+    public Node getNodeNearUser(Vector3 userPosition)
+    {
+        float minDistance = float.MaxValue;
+        Node nearestNode = new Node();
+        foreach (Node node in nodeMap)
+        {
+            float distance = Mathf.Sqrt(Mathf.Pow(node.position.x - userPosition.x, 2) +
+                                        Mathf.Pow(node.position.y - userPosition.y, 2) +
+                                        Mathf.Pow(node.position.z - userPosition.z, 2));
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestNode = node;
+            }
+        }
+        return nearestNode;
+    }
+
+    void LogNodes()
+    {
+        foreach (Node node in nodeMap)
+        {
+            Debug.Log(node.id + " (" + node.position.x + ", " + node.position.y + ", " + node.position.z + ")");
+        }
+    }
+
+    public void LogRoute(List<Node> route)
+    {
+        string output = "Route: ";
+
+        for (int i = 0; i < route.Count; i++)
+        {
+            output += route[i].id;
+
+            if (i < route.Count - 1) output += " -> ";
+        }
+
+        Debug.Log(output);
+    }
+
+    void LogLocations()
+    {
+        foreach (Location location in locations)
+        {
+            Debug.Log("Name: " + location.name + ", NodeID: " + location.node.id);
+        }
+    }
+
+    void LoadFromJson(string directory)
+    {
+        string filePath = Application.streamingAssetsPath + directory;
+
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            NodeMap jsonNodeMap = JsonUtility.FromJson<NodeMap>(jsonContent);
+
+            modelName = jsonNodeMap.modelname;
+            nodeMap = ConvertedJsonNodeMap(jsonNodeMap);
+            locations = ConvertedJsonLocations(jsonNodeMap.locations);
+
+            if (MapLoaded != null) MapLoaded();
+        }
+        else
+        {
+            Debug.LogError("Cannot load data from " + filePath);
+        }
+    }
+
+    public List<Node> ConvertedJsonNodeMap(NodeMap jsonNodeMap)
+    {
+        List<Node> nodeMap = new List<Node>();
+
+        foreach (JsonNode jsonNode in jsonNodeMap.nodes)
+        {
+            Node node = new Node();
+
+            node.id = jsonNode.id;
+            node.position = new Vector3(jsonNode.position.x, jsonNode.position.y, jsonNode.position.z);
+            node.adjacentNodeIDs = jsonNode.adjacentNodeIDs;
+
+            nodeMap.Add(node);
+        }
+
+        return nodeMap;
+    }
+
+    public List<Location> ConvertedJsonLocations(List<JsonLocation> jsonLocations)
+    {
+        List<Location> locations = new List<Location>();
+
+        foreach (JsonLocation jsonLocation in jsonLocations)
+        {
+            Location location = new Location();
+
+            location.name = jsonLocation.name;
+            location.node = GetNodeByID(jsonLocation.nodeID);
+
+            locations.Add(location);
+        }
+
+        return locations;
+    }
+
+    [Serializable]
+    public class NodeMap
+    {
+        public string title;
+        public string modelname;
+        public List<JsonNode> nodes;
+        public List<JsonLocation> locations;
+    }
+
+    [Serializable]
+    public class JsonNode
+    {
+        public int id;
+        public Positition position;
+        public List<int> adjacentNodeIDs;
+
+    }
+
+    [Serializable]
+    public class Positition
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [Serializable]
+    public class JsonLocation
+    {
+        public string name;
+        public int nodeID;
+    }
 }
